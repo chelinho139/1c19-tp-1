b296a4dfc18c3c3e6a8060cb9d5dff7996fe2d9c
crisnicogonzalez <cristian3629@gmail.com>
2019-04-13 16:04:03 -0300

    Server finished

--- py/app.py
+++ py/app.py
@@ -1,62 +1,69 @@
-from flask import Flask,send_from_directory
+from flask import Flask, send_from_directory
 import requests
 import time
-import datetime
 
-app = Flask(__name__, static_url_path='static')
+app = Flask(__name__, static_url_path='')
 
 
-SLEEP_TIME = 2000
+SLEEP_TIME_IN_SECONDS = 2
+INTENSIVE_COUNTER = 2000
 
 
 def intensive_op(foo):
+  print("foo {}".format(foo))
   foo = foo + 1
   return foo*foo
 
 
 #CASE 1
-@app.route("/",methods=["GET"])
+@app.route("/", methods=["GET"])
 def healt_check():
   return 'Doritos Dev - Node Test Server'
 
 #CASE 2
-@app.route("/timeout",methods=["GET"])
+@app.route("/timeout", methods=["GET"])
 def time_out():
-  time.sleep(SLEEP_TIME)
+  time.sleep(SLEEP_TIME_IN_SECONDS)
   return "OK"
 
 
 #CASE 3
-@app.route("/intensive",methods=["GET"])
+@app.route("/intensive", methods=["GET"])
 def intensive():
-  old_time = datetime.datetime.now()
+  old_time = time.time()
   delta = 0
   foo = 0
   counter = 0
-  while counter < SLEEP_TIME:
+  while counter < INTENSIVE_COUNTER:
+    #do anything calculate
     intensive_op(foo)
-    new_time = datetime.datetime.now()
-    delta = new_time.date() - old_time.date()
+    new_time = time.time()
+    delta = new_time - old_time
+    print("delta {}".format(counter))
     counter = counter + delta
+    print("counter {}".format(counter))
     oldTime = new_time
   return "OK"
 
 
 #CASE 4
-@app.route("/static",methods=["GET"])
+@app.route("/static", methods=["GET"])
 def return_static_file():
-  return send_from_directory("doritos.jpg")
+  return send_from_directory("static", "doritos.jpg")
 
 
 
 #CASE 5
-@app.route("/proxy",methods=["GET"])
+@app.route("/proxy", methods=["GET"])
 def proxy():
   print("GET /timeout")
-  res = requests.get("http:localhost:4000/timeout")
+  res = requests.get("http://localhost:5000/timeout")
   if res.status_code == 200:
     return "OK"
   return "NOT OK"
+
+
+app.run()
 
 
 
